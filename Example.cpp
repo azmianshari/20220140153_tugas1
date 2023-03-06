429c29e518e224704e2bc36bb739c787c1786b29
azmianshari <azmianshari123@gmail.com>
2023-03-06 15:34:50 +0700

    first

---
+++ Example.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double p,l,luas;
+    cout<<"masukan panjang = ";
+    cin>>p;
+    cout<<"masukan lebar";
+    cin>>l;
+    luas=p*l;
+    cout<<"luas persegi panjang adalah"<<luas;
+}
